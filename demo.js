862c71642b092e784f522f42a071551d2a9a5dc2
AromalP2002 <akhilaromal938@gmail.com>
2024-12-10 09:19:37 +0530

    ohay

--- demo.js
+++ demo.js
@@ -14,3 +14,60 @@
 //     i++
 // }
 // while(i<=10)
+
+// let l=[1,2,3,4,5]
+// let l={name:'anu',age:23}
+// for (i in l){
+//     console.log(i);
+// }
+
+// for(i in l){
+//     console.log(i);
+// }
+
+// let a='   welcome   '
+// console.log(a.length);
+// console.log(a.charAt(5));
+// console.log(a.concat(' ','helo'));
+// console.log(a.endsWith('e'));
+// console.log(a.startsWith('w'));
+// console.log(a.slice(0,4));
+// console.log(a.trim());
+// console.log(a.trimStart());
+// console.log(a.trimEnd());
+
+
+// l=[1,2,3,4,5]
+// // l.pop();
+// console.log(l);
+// console.log(l.length);
+// console.log(l.at(3));
+// // l.push([6,7]);
+// // console.log(l);
+// // l.shift()
+
+
+// // l.unshift(10)
+
+// l[1]=100
+// console.log(l);
+
+
+
+function sample(){
+    console.log('sample fun')
+
+}
+sample()
+
+let sample1=function(){
+    console.log('sample1');
+
+}
+sample1()
+
+let sample2=()=>{
+    console.log('sample2');
+
+}
+sample2()
