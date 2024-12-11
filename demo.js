113ca91535d3c1ab9bb182192ce69a837fd876d9
AromalP2002 <akhilaromal938@gmail.com>
2024-12-11 09:22:16 +0530

    done

--- demo.js
+++ demo.js
@@ -54,20 +54,77 @@
 
 
 
-function sample(){
-    console.log('sample fun')
+// function sample(){
+//     console.log('sample fun')
+
+// }
+// sample()
+
+// let sample1=function(){
+//     console.log('sample1');
+
+// }
+// sample1()
+
+// let sample2=()=>{
+//     console.log('sample2');
+
+// }
+// sample2()
+
+
+let data=[{id:'1',name:'aromal',age:22},{id:'2',name:'abin',age:23},{id:'3',name:'roshan',age:21}]
+function Display(){
+    data.forEach(element=>{
+        let tbody=document.querySelector("tbody")
+        let tr = document.createElement("tr")
+        let id_td = document.createElement("td")
+        id_td.innerHTML=element.id
+        tr.appendChild(id_td)
+        let name_td=document.createElement("td")
+        name_td.innerHTML=element.name
+        tr.appendChild(name_td)
+        let age_td =document.createElement("td")
+        age_td.innerHTML=element.age
+        tr.appendChild(age_td)
+
+
+        let edit_td = document.createElement("td")
+        let edit_btn = document.createElement("button")
+        edit_btn.innerHTML='Edit'
+
+        edit_btn.onclick=function(){
+           edit_form(element.id) 
+        }
+        edit_btn.appendChild(edit_btn)
+        tr.appendChild(edit_td)
+
+
+        tbody.appendChild(tr)
+    })
 
 }
-sample()
+document.getElementById("add_form").addEventListener('submit',function(event){
+    event.preventDefault()
+    let id=document.getElementById("id").value
+    let name=document.getElementById("name").value
+    let age=document.getElementById("age").value
+    data.push({id:id,name:name,age:age})
+    Display()
+})
+function edit_form(id){
+    document.getElementById("add_form").style.display='none'
+    document.getElementById("edit_form").style.display='block'
+    console.log(id);
+    let edit_data=data.find(user=>user.id==id)
+    console.log(edit_data);
+    document.getElementById("e_id").value.edit_data.id
+    document.getElementById("e_name").value.edit_data.name
+    document.getElementById("e_age").value.edit_data.age
 
-let sample1=function(){
-    console.log('sample1');
 
 }
-sample1()
 
-let sample2=()=>{
-    console.log('sample2');
 
-}
-sample2()
+
+Display()
