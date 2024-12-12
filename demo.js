35648a4605bd495ce0ed4964133d10a64aa30003
AromalP2002 <akhilaromal938@gmail.com>
2024-12-12 09:28:07 +0530

    done

--- demo.js
+++ demo.js
@@ -75,10 +75,12 @@
 
 let data=[{id:'1',name:'aromal',age:22},{id:'2',name:'abin',age:23},{id:'3',name:'roshan',age:21}]
 function Display(){
+    let tbody=document.querySelector("tbody")
+    tbody.innerHTML=''
     data.forEach(element=>{
-        let tbody=document.querySelector("tbody")
-        let tr = document.createElement("tr")
-        let id_td = document.createElement("td")
+        
+        let tr=document.createElement("tr")
+        let id_td=document.createElement("td")
         id_td.innerHTML=element.id
         tr.appendChild(id_td)
         let name_td=document.createElement("td")
@@ -92,38 +94,81 @@ function Display(){
         let edit_td = document.createElement("td")
         let edit_btn = document.createElement("button")
         edit_btn.innerHTML='Edit'
-
         edit_btn.onclick=function(){
+
            edit_form(element.id) 
         }
-        edit_btn.appendChild(edit_btn)
+        edit_td.appendChild(edit_btn)
         tr.appendChild(edit_td)
+
+
+
+        let del_td=document.createElement("td")
+        let del_btn=document.createElement("button")
+        del_btn.innerHTML='Delect'
+        del_btn.onclick=function(){
+            delete_data(element.id)
+        }
+        del_td.appendChild(del_btn)
+        tr.appendChild(del_td)
+
 
 
         tbody.appendChild(tr)
     })
 
 }
-document.getElementById("add_form").addEventListener('submit',function(event){
+
+
+function delete_data(id){
+    data=data.filter((user)=>{
+        if(user.id!=id){
+            return user
+        }
+    })
+    Display()
+}
+document.getElementById("edit_form").addEventListener("submit",function(event){
     event.preventDefault()
-    let id=document.getElementById("id").value
-    let name=document.getElementById("name").value
-    let age=document.getElementById("age").value
+    let id=document.getElementById9("e_id").value
+    let name=document.getElementById9("e_name").value
+    let age=document.getElementById9("e_age").value
     data.push({id:id,name:name,age:age})
     Display()
 })
+let edit_var
 function edit_form(id){
     document.getElementById("add_form").style.display='none'
     document.getElementById("edit_form").style.display='block'
     console.log(id);
     let edit_data=data.find(user=>user.id==id)
     console.log(edit_data);
-    document.getElementById("e_id").value.edit_data.id
-    document.getElementById("e_name").value.edit_data.name
-    document.getElementById("e_age").value.edit_data.age
+    document.getElementById("e_id").value=edit_data.id
+    document.getElementById("e_name").value=edit_data.name
+    document.getElementById("e_age").value=edit_data.age
+    edit_var=id
 
 
 }
+document.getElementById("edit_form").addEventListener("submit",function(event){
+    event.preventDefault()
+    let id=document.getElementById9("e_id").value
+    let name=document.getElementById9("e_name").value
+    let age=document.getElementById9("e_age").value
+
+    data=data.map((user)=>{
+        if(user.id==edit_var){
+            return{...user,id:id,name:name,age:age}
+
+        }
+        return user
+    })
+        document.getElementById("add_form").style.display='block'
+        document.getElementById("edit_form").style.display='none'
+
+    
+})
+
 
 
 
